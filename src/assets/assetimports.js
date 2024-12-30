68cce87ae6fff6d64fe3d52b237789dca8e9e7aa
MeetKhetani2003 <meetkhetani1111@gmail.com>
2024-12-30 11:53:43 +0530

    course update

--- src/assets/assetimports.js
+++ src/assets/assetimports.js
@@ -1,11 +1,35 @@
 import ai from './ailinks.png';
 import digital from './digital.webp';
+import digitalAbout from './digitalAbout.webp';
 import hero from './homeBg.png';
 import logo from './logo.png';
+import Tool1 from './Tool1.png';
+import Tool10 from './Tool10.svg';
+import Tool11 from './Tool11.svg';
+import Tool2 from './Tool2.png';
+import Tool3 from './Tool3.png';
+import Tool4 from './Tool4.svg';
+import Tool5 from './Tool5.svg';
+import Tool6 from './Tool6.svg';
+import Tool7 from './Tool7.svg';
+import Tool8 from './Tool8.svg';
+import Tool9 from './Tool9.svg';
 
 export const assets = {
   logo,
   hero,
   ai,
   digital,
+  digitalAbout,
+  Tool2,
+  Tool3,
+  Tool1,
+  Tool4,
+  Tool5,
+  Tool6,
+  Tool7,
+  Tool8,
+  Tool9,
+  Tool10,
+  Tool11,
 };
--- src/page/Courses/Course.jsx
+++ src/page/Courses/Course.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
 import { assets } from '@/assets/assetimports';
+import AboutDigital from '@/Components/Course/AboutDigital';
 import BreadcrumbLayout from '@/Components/BreadCrumbLayout';
+import CourseDetails from '@/Components/Course/CourseDetails';
+import CourseOfferings from '@/Components/Course/CourseOfferings';
 import Hero from '@/Components/Course/Hero';
+import ToolsMarquee from '@/Components/Course/Tools';
 
 const Course = () => {
   const breadcrumbItems = [
@@ -11,7 +15,7 @@ const Course = () => {
     { title: 'Digital Marketing' }, // No `href` for the current page
   ];
 
-  const courseDetails = [
+  const courseDetailsData = [
     { title: '29 December 2024', subtitle: 'Date of Commencement' },
     { title: '3 Months', subtitle: 'Duration' },
     { title: 'English', subtitle: 'Language' },
@@ -21,32 +25,20 @@ const Course = () => {
 
   return (
     <div>
-      {/* Hero Section */}
-      <Hero />
+      <div className='bg-blue-50 py-6'>
+        <Hero />
 
-      {/* EMI Information */}
-      <div className='max-w-7xl mx-auto pl-7 py-4 font-openSans text-gray-500 text-md font-semibold'>
-        <p>EMI options available</p>
-      </div>
-
-      {/* Course Details Section */}
-      <div className='max-w-7xl mx-auto bg-white rounded-lg shadow-lg p-6'>
-        <div className='grid grid-cols-1 md:grid-cols-5 gap-4'>
-          {courseDetails.map((item, index) => (
-            <div
-              key={index}
-              className='flex  flex-col md:flex-row justify-evenly items-start md:items-center pb-4 md:pb-0'
-            >
-              <div className='flex flex-col'>
-                <div className='text-2xl font-bold text-gray-800'>
-                  {item.title}
-                </div>
-                <div className='text-sm text-gray-500'>{item.subtitle}</div>
-              </div>
-            </div>
-          ))}
+        {/* EMI Information */}
+        <div className='max-w-7xl mx-auto pl-7 py-4 font-openSans text-gray-500 text-md font-semibold'>
+          <p>EMI options available</p>
         </div>
+
+        <CourseDetails courseDetails={courseDetailsData} />
       </div>
+      {/* Hero Section */}
+      <AboutDigital />
+      <CourseOfferings />
+      <ToolsMarquee />
     </div>
   );
 };
